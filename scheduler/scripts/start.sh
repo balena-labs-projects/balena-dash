742f9d2b600e9bbaef66dbc9d32de220c18fd93f
Chris Crocker-White <chriscw@balena.io>
2019-01-17 12:48:37 +0000

    Added cron default times

--- scheduler/scripts/start.sh
+++ scheduler/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-(crontab -l; echo "$BACKLIGHT_ON /usr/src/backlight_on.sh") | crontab -
-(crontab -l; echo "$BACKLIGHT_OFF /usr/src/backlight_off.sh") | crontab -
+(crontab -l; echo "${BACKLIGHT_ON:-0 8 * * *} /usr/src/backlight_on.sh") | crontab -
+(crontab -l; echo "${BACKLIGHT_OFF:-0 23 * * *} /usr/src/backlight_off.sh") | crontab -
 
 crond -f
