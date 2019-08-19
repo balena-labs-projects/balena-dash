40d50266293354cb7fcd37dbd4e8285879fe40c3
Anže Jenšterle <anze@anze.xyz>
2019-08-19 23:12:12 +0200

    var check for scheduler script
    Change-type: patch

--- scheduler/scripts/start.sh
+++ scheduler/scripts/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$ENABLE_BACKLIGHT_TIMER" -eq "1" ]
+if [ ! -z ${ENABLE_BACKLIGHT_TIMER+x} ] && [ "$ENABLE_BACKLIGHT_TIMER" -eq "1" ]
 then
   (crontab -l; echo "${BACKLIGHT_ON:-0 8 * * *} /usr/src/backlight_on.sh") | crontab -
   (crontab -l; echo "${BACKLIGHT_OFF:-0 23 * * *} /usr/src/backlight_off.sh") | crontab -
