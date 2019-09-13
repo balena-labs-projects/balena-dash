1f6d5f245cfd4c583e93886c0bb0aade16f45396
escoand <escoand@users.noreply.github.com>
2019-09-13 19:30:38 +0200

    add ability to switch HDMI displays with scheduler
    Change-type: minor

--- scheduler/scripts/backlight_off.sh
+++ scheduler/scripts/backlight_off.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo -n 1 > /sys/class/backlight/rpi_backlight/bl_power
+
+export PATH="$PATH:/opt/vc/bin/"
+export LD_LIBRARY_PATH="/opt/vc/lib/"
+
+output=$(tvservice -s)
+
+if [[ $output =~ "0x400000 [LCD]" ]]; then
+  echo -n 1 > /sys/class/backlight/rpi_backlight/bl_power
+else
+  echo standby 0 | cec-client -d 1 -s
+fi
