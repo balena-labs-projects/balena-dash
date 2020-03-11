a33c29d2667be30b34244ff46bf9d7420c82580f
Rahul Thakoor <rahul.thakoor@gmail.com>
2020-03-11 15:11:37 +0400

    Add Pi4 support using chromium

---
+++ kiosk/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pkill xinit
+
+sleep 3
+
+# delete the last line in xstart script and replace with new settings
+sed -i '$d' /home/chromium/xstart.sh
+
+echo "chromium-browser $FLAGS --app=$1  --window-size=$WINDOW_SIZE" >> /home/chromium/xstart.sh
+
+
+# run script as chromium user
+su -c 'export DISPLAY=:0 && startx /home/chromium/xstart.sh -- -nocursor' - chromium
