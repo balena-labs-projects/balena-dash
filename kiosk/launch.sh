3df2ebefa0a071bd476d97e8184f02a6277934c7
Phil Wilson <phil@balena.io>
2020-04-14 12:16:44 +0100

    Implemented SHOW_CURSOR envar to control WPE and Chromium cursor status. Previous WPE_BCMRPI_CURSOR still honoured to ensure backwards compatibility for WPE installations.
    Change-type: patch
    Signed-off-by: Phil Wilson <phil@balena.io>

--- kiosk/launch.sh
+++ kiosk/launch.sh
@@ -9,8 +9,19 @@ sed -i '$d' /home/chromium/xstart.sh
 
 echo "chromium-browser $FLAGS --app=$1  --window-size=$WINDOW_SIZE" >> /home/chromium/xstart.sh
 
+# make sure any lock on the Chromium profile is released
 chown -R chromium:chromium /usr/src/app/settings
 rm -f /usr/src/app/settings/SingletonLock
 
+#Set whether to show a cursor or not
+if [[ -z ${SHOW_CURSOR+x} ]]
+  then
+    export CURSOR='-- -nocursor'
+    echo "Disabling cursor"
+  else
+    export CURSOR=''
+    echo "Enabling cursor"
+fi
+
 # run script as chromium user
-su -c 'export DISPLAY=:0 && startx /home/chromium/xstart.sh -- -nocursor' - chromium
+su -c "export DISPLAY=:0 && startx /home/chromium/xstart.sh $CURSOR" - chromium
